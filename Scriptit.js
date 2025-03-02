6b4cf2964777f2e1a36b64f6923ac71ea03214ef
SantLeht <santleht@uef.fi>
2025-03-02 17:14:29 +0200

    Visa

--- Scriptit.js
+++ Scriptit.js
@@ -3,3 +3,25 @@ function togglemenu(){
     links.classList.toggle('active');
 
 }
+
+function tarkista(){
+    const oikeat = {
+        q1: "Mustavalkoinen",
+        q2: "Kilpi",
+        q3: "1"
+
+    };
+
+    let pisteet = 0;
+
+    for(let kysymys in oikeat){
+        const valittu = document.querySelector(`input[name="${kysymys}"]:checked`);
+        if(valittu && valittu.value == oikeat[kysymys]){
+            pisteet++;
+        }
+            
+
+    }
+    document.getElementById("tulos").innerText = `Sait ${pisteet} / 3 oikein!`;
+
+}
